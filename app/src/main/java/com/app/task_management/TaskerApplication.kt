2e7844ff8f551571e7fd10a3906f23a353d0b5f5
Nguyen Duc Hieu <dhieu03@gmail.com>
2023-05-18 08:07:21 +0700

    reverse Room version

--- app/src/main/java/com/app/task_management/TaskerApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.app.task_management
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class TaskerApplication: Application() {
-}
--- app/src/main/java/com/app/task_management/util/Constant.kt
+++
@@ -1,5 +0,0 @@
-package com.app.task_management.util
-
-object Constant {
-    var USER_COLLECTION = "user"
-}
